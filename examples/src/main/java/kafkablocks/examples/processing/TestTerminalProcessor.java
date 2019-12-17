d2e872ffa36df9c6dde05382721214a605fb8993
Konyaev Alexey <AKonyaev@croc.ru>
2019-12-17 16:15:50 +0300

    not working topology example (commented)

---
+++ examples/src/main/java/kafkablocks/examples/processing/TestTerminalProcessor.java
@@ -0,0 +1,21 @@
+//package kafkablocks.examples.processing;
+//
+//import kafkablocks.examples.events.PositionEvent;
+//import kafkablocks.processing.BaseTerminalEventProcessor;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Scope;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@Scope("prototype")
+//@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+//public class TestTerminalProcessor
+//        extends BaseTerminalEventProcessor<PositionEvent> {
+//
+//    @Override
+//    protected void processEvent(String key, PositionEvent positionEvent) {
+//        //todo
+//        // https://github.com/a-konyaev/kafka-blocks/issues/1
+//    }
+//}
